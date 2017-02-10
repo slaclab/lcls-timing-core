eb73371aeade414affe958ca682a17fa53b9919c
Larry Ruckman <ruckman@slac.stanford.edu>
2017-02-09 22:09:03 -0800

    fixed sensitivity list

--- LCLS-I/rtl/LclsMrTimingTriggerPulse.vhd
+++ LCLS-I/rtl/LclsMrTimingTriggerPulse.vhd
@@ -5,7 +5,7 @@
 -- Author     : Uros Legat  <ulegat@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2015-06-08
--- Last update: 2015-06-08
+-- Last update: 2017-02-09
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -30,6 +30,7 @@
 -- may be copied, modified, propagated, or distributed except according to 
 -- the terms contained in the LICENSE.txt file.
 -------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
@@ -38,54 +39,43 @@ use ieee.std_logic_arith.all;
 use work.StdRtlPkg.all;
 use work.AxiLitePkg.all;
 
-
 entity LclsMrTimingTriggerPulse is
    generic (
-      TPD_G             : time     := 1 ns;
-      AXI_ERROR_RESP_G  : slv(1 downto 0)       := AXI_RESP_DECERR_C;
-      DELAY_WIDTH_G     : integer range 1 to 32 := 32;
-      PULSE_WIDTH_G     : integer range 1 to 32 := 32
-   );
+      TPD_G            : time                  := 1 ns;
+      AXI_ERROR_RESP_G : slv(1 downto 0)       := AXI_RESP_DECERR_C;
+      DELAY_WIDTH_G    : integer range 1 to 32 := 32;
+      PULSE_WIDTH_G    : integer range 1 to 32 := 32);
    port (
-
       -- Timing clock
-      clk          : in  sl;
-      rst          : in  sl;
-      
-      -- AXI lite
-      axilClk : in sl;
-      axilRst : in sl;
+      clk : in sl;
+      rst : in sl;
+      -- AXI-Lite Interface
+      axilClk         : in  sl;
+      axilRst         : in  sl;
       axilReadMaster  : in  AxiLiteReadMasterType  := AXI_LITE_READ_MASTER_INIT_C;
       axilReadSlave   : out AxiLiteReadSlaveType;
       axilWriteMaster : in  AxiLiteWriteMasterType := AXI_LITE_WRITE_MASTER_INIT_C;
       axilWriteSlave  : out AxiLiteWriteSlaveType;
-
       -- Opcodes (events)
-      opcodes_i    : in  slv(255 downto 0);
-      strobe_i     : in  sl;
-      
+      opcodes_i : in slv(255 downto 0);
+      strobe_i  : in sl;
       -- Trigger pulse output 
-      pulse_o    : out  sl
-   );
+      pulse_o : out sl);
 end LclsMrTimingTriggerPulse;
 
 architecture rtl of LclsMrTimingTriggerPulse is
 
    type StateType is (
-      --
       WAIT_TRIG_S,
       WAIT_DELAY_S,
-      WAIT_WIDTH_S
-   );
+      WAIT_WIDTH_S);
 
    type RegType is record
       cnt      : slv(31 downto 0);
       pulse    : sl;
       delayReg : slv(DELAY_WIDTH_G-1 downto 0);
       widthReg : slv(PULSE_WIDTH_G-1 downto 0);
-      
-      -- State Machine
-      state   : StateType;
+      state : StateType;
    end record RegType;
 
    constant REG_INIT_C : RegType := (
@@ -93,89 +83,82 @@ architecture rtl of LclsMrTimingTriggerPulse is
       pulse    => '0',
       delayReg => (others => '0'),
       widthReg => (others => '0'),
-      -- State machine       
-      state => WAIT_TRIG_S
-   );
-   
-   -- Internal signals
+      state    => WAIT_TRIG_S);
+
    signal r   : RegType := REG_INIT_C;
    signal rin : RegType;
 
-   -- Register controls
    signal s_opcodesReg : slv(255 downto 0);
    signal s_delayReg   : slv(DELAY_WIDTH_G-1 downto 0);
    signal s_widthReg   : slv(PULSE_WIDTH_G-1 downto 0);
    signal s_polarity   : sl;
------
+
 begin
-   
-   U_LclsMrTimingTriggerPulseReg: entity work.LclsMrTimingTriggerPulseReg
-   generic map (
-      TPD_G            => TPD_G,
-      AXI_ERROR_RESP_G => AXI_ERROR_RESP_G,
-      DELAY_WIDTH_G    => DELAY_WIDTH_G,
-      PULSE_WIDTH_G    => PULSE_WIDTH_G)
-   port map (
-      axiclk_i        => axilClk,
-      axirst_i        => axilRst,
-      axilReadMaster  => axilReadMaster,
-      axilReadSlave   => axilReadSlave,
-      axilWriteMaster => axilWriteMaster,
-      axilWriteSlave  => axilWriteSlave,
-      devclk_i        => clk,
-      devrst_i        => rst,
-      opcodesReg_o    => s_opcodesReg,
-      delayReg_o      => s_delayReg,
-      widthReg_o      => s_widthReg,
-      polarity_o      => s_polarity);
 
-   
-   ----------------------------------------------------------------------------------------------- 
-   comb : process (r, rst, opcodes_i, strobe_i, s_opcodesReg, s_delayReg, s_widthReg) is
-      
+   U_Reg : entity work.LclsMrTimingTriggerPulseReg
+      generic map (
+         TPD_G            => TPD_G,
+         AXI_ERROR_RESP_G => AXI_ERROR_RESP_G,
+         DELAY_WIDTH_G    => DELAY_WIDTH_G,
+         PULSE_WIDTH_G    => PULSE_WIDTH_G)
+      port map (
+         axiclk_i        => axilClk,
+         axirst_i        => axilRst,
+         axilReadMaster  => axilReadMaster,
+         axilReadSlave   => axilReadSlave,
+         axilWriteMaster => axilWriteMaster,
+         axilWriteSlave  => axilWriteSlave,
+         devclk_i        => clk,
+         devrst_i        => rst,
+         opcodesReg_o    => s_opcodesReg,
+         delayReg_o      => s_delayReg,
+         widthReg_o      => s_widthReg,
+         polarity_o      => s_polarity);
+
+   comb : process (r, rst, s_polarity, opcodes_i, strobe_i, s_opcodesReg, s_delayReg, s_widthReg) is
       variable v : RegType;
-
    begin
+      -- Latch the current value
       v := r;
 
+      -- State machine
       case r.state is
          ----------------------------------------------------------------------
          when WAIT_TRIG_S =>
-            v.cnt   := (others => '0');
-            v.pulse := s_polarity; -- Pulse NOT active
+            -- Save configurations
+            v.pulse    := s_polarity;   -- Pulse NOT active
             v.delayReg := s_delayReg;
             v.widthReg := s_widthReg;
-            
-            -- 
-            if ( (opcodes_i and s_opcodesReg) /= 0 and
-                 strobe_i     = '1'
-            ) then
-              --
-              v.state      := WAIT_DELAY_S;
+            -- Check for trigger
+            if ((opcodes_i and s_opcodesReg) /= 0) and (strobe_i = '1') then
+               -- Next state
+               v.state := WAIT_DELAY_S;
             end if;
          ----------------------------------------------------------------------
          when WAIT_DELAY_S =>
-            v.cnt := r.cnt + 1;
-            v.pulse := s_polarity; -- Pulse NOT active
-            -- 
+            -- Increment the counter
+            v.cnt   := r.cnt + 1;
+            -- NOT asserted the pulse
+            v.pulse := s_polarity;      -- Pulse NOT active
+            -- Check the counter
             if r.cnt = r.delayReg then
-               v.cnt    := (others => '0');
-               v.state  := WAIT_WIDTH_S;
+               v.cnt   := (others => '0');
+               -- Next state
+               v.state := WAIT_WIDTH_S;
             end if;
          ----------------------------------------------------------------------
          when WAIT_WIDTH_S =>
-            v.cnt := r.cnt + 1;
-            v.pulse := not s_polarity; -- Pulse active
-            -- 
+            -- Increment the counter
+            v.cnt   := r.cnt + 1;
+            -- Asserted the pulse
+            v.pulse := not(s_polarity);  -- Pulse active
+            -- Check the counter
             if r.cnt = r.widthReg then
-               v.state      := WAIT_TRIG_S;
+               -- Reset the counter
+               v.cnt      := (others => '0');
+               -- Next state
+               v.state := WAIT_TRIG_S;
             end if;
-
-         ----------------------------------------------------------------------
-         when others =>
-            --
-            v := REG_INIT_C;
-           
       ----------------------------------------------------------------------
       end case;
 
@@ -183,11 +166,13 @@ begin
       if (rst = '1') then
          v := REG_INIT_C;
       end if;
+
+      -- Register the variable for next clock cycle
+      rin <= v;
       
-      -- Output assignment
-      rin     <= v;
+      -- Outputs
       pulse_o <= r.pulse;
-      
+
    end process comb;
 
    seq : process (clk) is
@@ -196,5 +181,5 @@ begin
          r <= rin after TPD_G;
       end if;
    end process seq;
-   ---------------------------------------------------------------------
-end architecture rtl;
+
+end  rtl;
--- LCLS-I/rtl/LclsMrTimingTriggerPulseReg.vhd
+++ LCLS-I/rtl/LclsMrTimingTriggerPulseReg.vhd
@@ -1,19 +1,11 @@
 -------------------------------------------------------------------------------
--- Title      : Axi-lite interface for Timing trigger pulse 
--------------------------------------------------------------------------------
 -- File       : LclsMrTimingTriggerPulseReg.vhd
 -- Author     : Uros Legat  <ulegat@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory (Cosylab)
 -- Created    : 2015-04-15
--- Last update: 2015-04-15
--- Platform   : 
--- Standard   : VHDL'93/02
+-- Last update: 2017-02-09
 -------------------------------------------------------------------------------
--- Description:  Register decoding
---               0x00 - 0x07 (RW)- Opcode mask
---               0x08        (RW)- Pulse delay
---               0x09        (RW)- Pulse width
---               0x0A        (RW)- Bit0: Polarity
+-- Description:  
 -------------------------------------------------------------------------------
 -- This file is part of 'LCLS2 LLRF Development'.
 -- It is subject to the license terms in the LICENSE.txt file found in the 
@@ -33,128 +25,73 @@ use work.AxiLitePkg.all;
 
 entity LclsMrTimingTriggerPulseReg is
    generic (
-   -- General Configurations
-      TPD_G               : time             := 1 ns;
-      AXI_ERROR_RESP_G    : slv(1 downto 0)  := AXI_RESP_SLVERR_C; 
-      AXI_ADDR_WIDTH_G    : positive         := 8;
-      DELAY_WIDTH_G : integer range 1 to 32  := 16;
-      PULSE_WIDTH_G : integer range 1 to 32  := 16
-   );    
+      -- General Configurations
+      TPD_G            : time                  := 1 ns;
+      AXI_ERROR_RESP_G : slv(1 downto 0)       := AXI_RESP_SLVERR_C;
+      AXI_ADDR_WIDTH_G : positive              := 8;
+      DELAY_WIDTH_G    : integer range 1 to 32 := 16;
+      PULSE_WIDTH_G    : integer range 1 to 32 := 16);
    port (
-    -- AXI Clk
-      axiClk_i : in sl;
-      axiRst_i : in sl;
-
-    -- Axi-Lite Register Interface (axiClk domain)
+      -- AXI Clk
+      axiClk_i        : in  sl;
+      axiRst_i        : in  sl;
+      -- Axi-Lite Register Interface (axiClk domain)
       axilReadMaster  : in  AxiLiteReadMasterType  := AXI_LITE_READ_MASTER_INIT_C;
       axilReadSlave   : out AxiLiteReadSlaveType;
       axilWriteMaster : in  AxiLiteWriteMasterType := AXI_LITE_WRITE_MASTER_INIT_C;
       axilWriteSlave  : out AxiLiteWriteSlaveType;
- 
-   -- Dev Clk
-      devClk_i          : in  sl;
-      devRst_i          : in  sl;
-
-   -- Registers
-      opcodesReg_o : out slv(255 downto 0);
-      delayReg_o   : out slv(DELAY_WIDTH_G-1 downto 0);
-      widthReg_o   : out slv(PULSE_WIDTH_G-1 downto 0);
-      polarity_o   : out sl
-   );   
+      -- Dev Clk
+      devClk_i        : in  sl;
+      devRst_i        : in  sl;
+      -- Registers
+      opcodesReg_o    : out slv(255 downto 0);
+      delayReg_o      : out slv(DELAY_WIDTH_G-1 downto 0);
+      widthReg_o      : out slv(PULSE_WIDTH_G-1 downto 0);
+      polarity_o      : out sl);
 end LclsMrTimingTriggerPulseReg;
 
 architecture rtl of LclsMrTimingTriggerPulseReg is
 
    type RegType is record
-      -- 
-      opcodes : slv(255 downto 0);
-      delay   : slv(DELAY_WIDTH_G-1 downto 0);
-      width   : slv(PULSE_WIDTH_G-1 downto 0);
-      polarity: sl;
-
-      -- AXI lite
+      opcodes        : slv(255 downto 0);
+      delay          : slv(DELAY_WIDTH_G-1 downto 0);
+      pulseWidth     : slv(PULSE_WIDTH_G-1 downto 0);
+      polarity       : sl;
       axilReadSlave  : AxiLiteReadSlaveType;
       axilWriteSlave : AxiLiteWriteSlaveType;
    end record;
-   
+
    constant REG_INIT_C : RegType := (
-      opcodes  => x"0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000", 
-      delay    => (others =>'0'),      
-      width    => (others =>'0'),
-      polarity => '0', 
-     
+      opcodes        => (others => '0'),
+      delay          => (others => '0'),
+      pulseWidth     => (others => '0'),
+      polarity       => '0',
       axilReadSlave  => AXI_LITE_READ_SLAVE_INIT_C,
       axilWriteSlave => AXI_LITE_WRITE_SLAVE_INIT_C);
 
    signal r   : RegType := REG_INIT_C;
    signal rin : RegType;
-   
-   -- Integer address
-   signal s_RdAddr: natural := 0;
-   signal s_WrAddr: natural := 0;
 
 begin
-   
-   -- Convert address to integer (lower two bits of address are always '0')
-   s_RdAddr <= conv_integer( axilReadMaster.araddr(AXI_ADDR_WIDTH_G-1 downto 2));
-   s_WrAddr <= conv_integer( axilWriteMaster.awaddr(AXI_ADDR_WIDTH_G-1 downto 2)); 
-   
-   comb : process (axilReadMaster, axilWriteMaster, r, axiRst_i, s_RdAddr, s_WrAddr) is
-      variable v             : RegType;
-      variable axilStatus    : AxiLiteStatusType;
-      variable axilWriteResp : slv(1 downto 0);
-      variable axilReadResp  : slv(1 downto 0);
+
+   comb : process (axiRst_i, axilReadMaster, axilWriteMaster, r) is
+      variable v      : RegType;
+      variable regCon : AxiLiteEndPointType;
    begin
       -- Latch the current value
       v := r;
-      
-      ----------------------------------------------------------------------------------------------
-      -- Axi-Lite interface
-      ----------------------------------------------------------------------------------------------
-      axiSlaveWaitTxn(axilWriteMaster, axilReadMaster, v.axilWriteSlave, v.axilReadSlave, axilStatus);
 
-      if (axilStatus.writeEnable = '1') then
-         axilWriteResp := ite(axilWriteMaster.awaddr(1 downto 0) = "00", AXI_RESP_OK_C, AXI_ERROR_RESP_G);
-         case (s_WrAddr) is
-            when 16#00# to 16#07# => -- ADDR (0x0-0x1C)
-               for i in 7 downto 0 loop
-                  if (axilWriteMaster.awaddr(4 downto 2) = i) then
-                     v.opcodes((32*i + 31) downto (32*i)) := axilWriteMaster.wdata;
-                  end if;
-               end loop; 
-            when 16#08# => -- ADDR (20)
-               v.delay  := axilWriteMaster.wdata(DELAY_WIDTH_G-1 downto 0);                
-            when 16#09# => -- ADDR (24)
-               v.width  := axilWriteMaster.wdata(PULSE_WIDTH_G-1 downto 0);
-            when 16#0A# => -- ADDR (28)
-               v.polarity  := axilWriteMaster.wdata(0);
-            when others =>
-               axilWriteResp     := AXI_ERROR_RESP_G;
-         end case;
-         axiSlaveWriteResponse(v.axilWriteSlave);
-      end if;
+      -- Determine the AXI-Lite transaction type
+      axiSlaveWaitTxn(regCon, axilWriteMaster, axilReadMaster, v.axilWriteSlave, v.axilReadSlave);
 
-      if (axilStatus.readEnable = '1') then
-         axilReadResp := ite(axilReadMaster.araddr(1 downto 0) = "00", AXI_RESP_OK_C, AXI_ERROR_RESP_G);
-         v.axilReadSlave.rdata := (others => '0');
-         case (s_RdAddr) is
-            when 16#00# to 16#07# =>  -- ADDR (0x0-0x1C)
-               for i in 7 downto 0 loop
-                  if (axilReadMaster.araddr(4 downto 2) = i) then
-                     v.axilReadSlave.rdata := r.opcodes((32*i + 31) downto (32*i));
-                  end if;
-               end loop;
-            when 16#08# =>  -- ADDR (20)
-               v.axilReadSlave.rdata(DELAY_WIDTH_G-1 downto 0)  := r.delay;               
-            when 16#09# =>  -- ADDR (24)
-               v.axilReadSlave.rdata(PULSE_WIDTH_G-1 downto 0)  := r.width;
-            when 16#0A# =>  -- ADDR (28)
-               v.axilReadSlave.rdata(0)  := r.polarity;               
-            when others =>
-               axilReadResp    := AXI_ERROR_RESP_G;
-         end case;
-         axiSlaveReadResponse(v.axilReadSlave);
-      end if;
+      -- Map the registers
+      axiSlaveRegisterR(regCon, x"00", 0, v.opcodes);
+      axiSlaveRegisterR(regCon, x"20", 0, v.delay);
+      axiSlaveRegisterR(regCon, x"24", 0, v.pulseWidth);
+      axiSlaveRegisterR(regCon, x"28", 0, v.polarity);
+
+      -- Closeout the AXI-Lite transaction
+      axiSlaveDefault(regCon, v.axilWriteSlave, v.axilReadSlave, AXI_ERROR_RESP_G);
 
       -- Reset
       if (axiRst_i = '1') then
@@ -167,7 +104,7 @@ begin
       -- Outputs
       axilReadSlave  <= r.axilReadSlave;
       axilWriteSlave <= r.axilWriteSlave;
-      
+
    end process comb;
 
    seq : process (axiClk_i) is
@@ -178,50 +115,41 @@ begin
    end process seq;
 
    SyncFifo_OUT0 : entity work.SynchronizerFifo
-   generic map (
-      TPD_G        => TPD_G,
-      DATA_WIDTH_G => 256
-   )
-   port map (
-      wr_clk => axiClk_i,
-      din    => r.opcodes,
-      rd_clk => devClk_i,
-      dout   => opcodesReg_o
-   );
-   
+      generic map (
+         TPD_G        => TPD_G,
+         DATA_WIDTH_G => 256)
+      port map (
+         wr_clk => axiClk_i,
+         din    => r.opcodes,
+         rd_clk => devClk_i,
+         dout   => opcodesReg_o);
+
    SyncFifo_OUT1 : entity work.SynchronizerFifo
-   generic map (
-      TPD_G        => TPD_G,
-      DATA_WIDTH_G => DELAY_WIDTH_G
-   )
-   port map (
-      wr_clk => axiClk_i,
-      din    => r.delay,
-      rd_clk => devClk_i,
-      dout   => delayReg_o
-   );
-      
+      generic map (
+         TPD_G        => TPD_G,
+         DATA_WIDTH_G => DELAY_WIDTH_G)
+      port map (
+         wr_clk => axiClk_i,
+         din    => r.delay,
+         rd_clk => devClk_i,
+         dout   => delayReg_o);
+
    SyncFifo_OUT2 : entity work.SynchronizerFifo
-   generic map (
-      TPD_G        => TPD_G,
-      DATA_WIDTH_G => PULSE_WIDTH_G
-   )
-   port map (
-      wr_clk => axiClk_i,
-      din    => r.width,
-      rd_clk => devClk_i,
-      dout   => widthReg_o
-   );
-   
+      generic map (
+         TPD_G        => TPD_G,
+         DATA_WIDTH_G => PULSE_WIDTH_G)
+      port map (
+         wr_clk => axiClk_i,
+         din    => r.pulseWidth,
+         rd_clk => devClk_i,
+         dout   => widthReg_o);
+
    Sync_OUT3 : entity work.Synchronizer
-   generic map (
-      TPD_G        => TPD_G
-   )
-    port map (
-      dataIn    => r.polarity,
-      clk       => devClk_i,
-      dataOut   => polarity_o
-   );
-   
----------------------------------------------------------------------
+      generic map (
+         TPD_G => TPD_G)
+      port map (
+         dataIn  => r.polarity,
+         clk     => devClk_i,
+         dataOut => polarity_o);
+
 end rtl;
