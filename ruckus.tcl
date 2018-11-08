8e7c6e34f27dec1d2626935acc9fe0b42c876a8d
Larry Ruckman <ruckman@slac.stanford.edu>
2018-11-08 09:19:23 -0800

    bug fixes for SURF v1.9.0

--- ruckus.tcl
+++ ruckus.tcl
@@ -6,8 +6,8 @@ if { [VersionCheck 2016.4 ] < 0 } {
    exit -1
 }
 
-if { [SubmoduleCheck {ruckus} {1.5.12} ] < 0 } {exit -1}
-if { [SubmoduleCheck {surf}   {1.7.1}  ] < 0 } {exit -1}
+if { [SubmoduleCheck {ruckus} {1.7.0} ] < 0 } {exit -1}
+if { [SubmoduleCheck {surf}   {1.9.0} ] < 0 } {exit -1}
 
 # Load ruckus files
 loadRuckusTcl "$::DIR_PATH/LCLS-I"  "quiet"
