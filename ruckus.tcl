c966c0d697d7c182d118462909a85a8737d5d7e1
Larry Ruckman <ruckman@slac.stanford.edu>
2018-11-08 11:05:50 -0800

    adding OVERRIDE_SUBMODULE_LOCKS feature

--- ruckus.tcl
+++ ruckus.tcl
@@ -6,8 +6,16 @@ if { [VersionCheck 2016.4 ] < 0 } {
    exit -1
 }
 
-if { [SubmoduleCheck {ruckus} {1.7.0} ] < 0 } {exit -1}
-if { [SubmoduleCheck {surf}   {1.9.0} ] < 0 } {exit -1}
+# Check for submodule tagging
+if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
+   if { [SubmoduleCheck {ruckus}           {1.7.0}  "mustBeExact" ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}             {1.9.0}  "mustBeExact" ] < 0 } {exit -1}
+} else {
+   puts "\n\n*********************************************************"
+   puts "OVERRIDE_SUBMODULE_LOCKS != 0"
+   puts "Ignoring the submodule locks in amc-carrier-core/ruckus.tcl"
+   puts "*********************************************************\n\n"
+}
 
 # Load ruckus files
 loadRuckusTcl "$::DIR_PATH/LCLS-I"  "quiet"
