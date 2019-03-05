1745656eb4b08e506466913f7a21a13371597110
Larry Ruckman <ruckman@slac.stanford.edu>
2019-03-05 10:50:20 -0800

    updating register modules for 256 Byte/ch and 4096 stride

--- LCLS-II/evr/rtl/EvrV2TrigReg.vhd
+++ LCLS-II/evr/rtl/EvrV2TrigReg.vhd
@@ -5,7 +5,7 @@
 -- Author     : Matt Weaver <weaver@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2016-01-04
--- Last update: 2018-02-15
+-- Last update: 2019-03-05
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -24,110 +24,126 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
-use ieee.NUMERIC_STD.all;
 
 use work.StdRtlPkg.all;
 use work.AxiLitePkg.all;
 use work.EvrV2Pkg.all;
 
 entity EvrV2TrigReg is
-  generic (
-    TPD_G      : time    := 1 ns;
-    TRIGGERS_C : integer := 1;
-    USE_TAP_C  : boolean := false );
-  port (
-    -- AXI-Lite and IRQ Interface
-    axiClk              : in  sl;
-    axiRst              : in  sl;
-    axilWriteMaster     : in  AxiLiteWriteMasterType;
-    axilWriteSlave      : out AxiLiteWriteSlaveType;
-    axilReadMaster      : in  AxiLiteReadMasterType;
-    axilReadSlave       : out AxiLiteReadSlaveType;
-    -- configuration
-    triggerConfig       : out EvrV2TriggerConfigArray(TRIGGERS_C-1 downto 0);
-    delay_rd            : in  Slv6Array(TRIGGERS_C-1 downto 0) := (others=>"000000") );
+   generic (
+      TPD_G      : time    := 1 ns;
+      TRIGGERS_C : integer := 1;
+      USE_TAP_C  : boolean := false);
+   port (
+      -- AXI-Lite and IRQ Interface
+      axiClk          : in  sl;
+      axiRst          : in  sl;
+      axilWriteMaster : in  AxiLiteWriteMasterType;
+      axilWriteSlave  : out AxiLiteWriteSlaveType;
+      axilReadMaster  : in  AxiLiteReadMasterType;
+      axilReadSlave   : out AxiLiteReadSlaveType;
+      -- configuration
+      triggerConfig   : out EvrV2TriggerConfigArray(TRIGGERS_C-1 downto 0);
+      delay_rd        : in  Slv6Array(TRIGGERS_C-1 downto 0) := (others => "000000"));
 end EvrV2TrigReg;
 
-architecture mapping of EvrV2TrigReg is
+architecture rtl of EvrV2TrigReg is
 
-  type RegType is record
-    axilReadSlave  : AxiLiteReadSlaveType;
-    axilWriteSlave : AxiLiteWriteSlaveType;
-    triggerConfig  : EvrV2TriggerConfigArray(TRIGGERS_C-1 downto 0);
-    loadShift      : Slv4Array(TRIGGERS_C-1 downto 0);
-  end record;
-  constant REG_INIT_C : RegType := (
-    axilReadSlave  => AXI_LITE_READ_SLAVE_INIT_C,
-    axilWriteSlave => AXI_LITE_WRITE_SLAVE_INIT_C,
-    triggerConfig  => (others=>EVRV2_TRIGGER_CONFIG_INIT_C),
-    loadShift      => (others=>(others=>'0')) );
-  signal r   : RegType := REG_INIT_C;
-  signal rin : RegType;
+   type RegType is record
+      axilReadSlave  : AxiLiteReadSlaveType;
+      axilWriteSlave : AxiLiteWriteSlaveType;
+      triggerConfig  : EvrV2TriggerConfigArray(TRIGGERS_C-1 downto 0);
+      loadShift      : Slv4Array(TRIGGERS_C-1 downto 0);
+   end record;
+   constant REG_INIT_C : RegType := (
+      axilReadSlave  => AXI_LITE_READ_SLAVE_INIT_C,
+      axilWriteSlave => AXI_LITE_WRITE_SLAVE_INIT_C,
+      triggerConfig  => (others => EVRV2_TRIGGER_CONFIG_INIT_C),
+      loadShift      => (others => (others => '0')));
 
-begin  -- mapping
+   signal r   : RegType := REG_INIT_C;
+   signal rin : RegType;
 
-  triggerConfig  <= r.triggerConfig;
-  axilReadSlave  <= r.axilReadSlave;
-  axilWriteSlave <= r.axilWriteSlave;
+begin
 
-  process (axiClk)
-  begin  -- process
-    if rising_edge(axiClk) then
-      r <= rin;
-    end if;
-  end process;
+   comb : process (axiRst, axilReadMaster, axilWriteMaster, delay_rd, r)
+      variable v      : RegType;
+      variable axilEp : AxiLiteEndpointType;
+      variable i      : natural;
+   begin
+      -- Latch the current value
+      v := r;
 
-  process (r,axilReadMaster,axilWriteMaster,axiRst,delay_rd)
-    variable v : RegType;
-    variable axilStatus : AxiLiteStatusType;
-    procedure axilSlaveRegisterW (addr : in slv; offset : in integer; reg : inout slv) is
-    begin
-      axiSlaveRegister(axilWriteMaster, axilReadMaster, v.axilWriteSlave, v.axilReadSlave, axilStatus, addr, offset, reg);
-    end procedure;
-    procedure axilSlaveRegisterW (addr : in slv; offset : in integer; reg : inout sl) is
-    begin
-      axiSlaveRegister(axilWriteMaster, axilReadMaster, v.axilWriteSlave, v.axilReadSlave, axilStatus, addr, offset, reg);
-    end procedure;
-    procedure axilSlaveDefault (
-      axilResp : in slv(1 downto 0)) is
-    begin
-      axiSlaveDefault(axilWriteMaster, axilReadMaster, v.axilWriteSlave, v.axilReadSlave, axilStatus, axilResp);
-    end procedure;
-  begin  -- process
-    v  := r;
-    axiSlaveWaitTxn(axilWriteMaster, axilReadMaster, v.axilWriteSlave, v.axilReadSlave, axilStatus);
+      -- Determine the transaction type
+      axiSlaveWaitTxn(axilEp, axilWriteMaster, axilReadMaster, v.axilWriteSlave, v.axilReadSlave);
 
-    for i in 0 to TRIGGERS_C-1 loop
-      axilSlaveRegisterW(slv(conv_unsigned(i*256,17)),   0, v.triggerConfig(i).channel);
-      axilSlaveRegisterW(slv(conv_unsigned(i*256,17)),  16, v.triggerConfig(i).polarity);
-      axilSlaveRegisterW(slv(conv_unsigned(i*256,17)),  31, v.triggerConfig(i).enabled);
-      axilSlaveRegisterW(slv(conv_unsigned(4+i*256,17)),   0, v.triggerConfig(i).delay);
-      axilSlaveRegisterW(slv(conv_unsigned(8+i*256,17)),   0, v.triggerConfig(i).width);
 
-      if USE_TAP_C then
-        --  Special handling of delay tap
-        v.triggerConfig(i).loadTap := r.loadShift(i)(3);
-        v.loadShift(i) := r.loadShift(i)(2 downto 0) & '0';
-        if (axilStatus.readEnable = '1') then
-          if (std_match(axilReadMaster.araddr(16 downto 0), toSlv(12+i*256,17))) then
-            v.axilReadSlave.rdata(31 downto 6) := (others=>'0');
-            v.axilReadSlave.rdata( 5 downto 0) := delay_rd(i);
-            axiSlaveReadResponse(v.axilReadSlave);
-          end if;
-        end if;
+      -- Loop through the channels
+      for i in 0 to TRIGGERS_C-1 loop
+         axiSlaveRegister(axilEp, toSlv(i*256 + 0, 12), 0, v.triggerConfig(i).channel);
+         axiSlaveRegister(axilEp, toSlv(i*256 + 0, 12), 16, v.triggerConfig(i).polarity);
+         axiSlaveRegister(axilEp, toSlv(i*256 + 0, 12), 31, v.triggerConfig(i).enabled);
+         axiSlaveRegister(axilEp, toSlv(i*256 + 4, 12), 0, v.triggerConfig(i).delay);
+         axiSlaveRegister(axilEp, toSlv(i*256 + 8, 12), 0, v.triggerConfig(i).width);
 
-        if (axilStatus.writeEnable = '1') then
-          if (std_match(axilWriteMaster.awaddr(16 downto 0), toSlv(12+i*256,17))) then
-            v.triggerConfig(i).delayTap := axilWriteMaster.wdata(5 downto 0);
-            axiSlaveWriteResponse(v.axilWriteSlave);
-            v.loadShift(i)(0) := '1';
-          end if;
-        end if;
+         --  Special handling of delay tap
+         if USE_TAP_C then
+
+            -- Shift Register
+            v.triggerConfig(i).loadTap := r.loadShift(i)(3);
+            v.loadShift(i)             := r.loadShift(i)(2 downto 0) & '0';
+
+            -- Check for read transaction
+            if (axilEp.axiStatus.readEnable = '1') then
+
+               -- Check the Address
+               if (StdMatch(axilReadMaster.araddr(11 downto 0), toSlv(i*256 + 12, 12))) then
+                  v.axilReadSlave.rdata(31 downto 6) := (others => '0');
+                  v.axilReadSlave.rdata(5 downto 0)  := delay_rd(i);
+                  axiSlaveReadResponse(v.axilReadSlave);
+               end if;
+
+            end if;
+
+            -- Check for Write transaction
+            if (axilEp.axiStatus.writeEnable = '1') then
+
+               -- Check the Address
+               if (StdMatch(axilWriteMaster.awaddr(11 downto 0), toSlv(i*256 + 12, 12))) then
+                  v.triggerConfig(i).delayTap := axilWriteMaster.wdata(5 downto 0);
+                  axiSlaveWriteResponse(v.axilWriteSlave);
+                  v.loadShift(i)(0)           := '1';
+               end if;
+
+            end if;
+
+         end if;
+
+      end loop;
+
+      -- Close the transaction
+      axiSlaveDefault(axilEp, v.axilWriteSlave, v.axilReadSlave, AXI_RESP_OK_C);
+
+      -- Outputs 
+      axilReadSlave  <= r.axilReadSlave;
+      axilWriteSlave <= r.axilWriteSlave;
+      triggerConfig  <= r.triggerConfig;
+
+      -- Reset
+      if (axiRst = '1') then
+         v := REG_INIT_C;
       end if;
-      
-    end loop;   -- i
-    axilSlaveDefault(AXI_RESP_OK_C);
-    rin <= v;
-  end process;
 
-end mapping;
+      -- Register the variable for next clock cycle
+      rin <= v;
+
+   end process comb;
+
+   seq : process (axiClk) is
+   begin
+      if (rising_edge(axiClk)) then
+         r <= rin after TPD_G;
+      end if;
+   end process seq;
+
+end architecture rtl;
