7cca76b076bbdf79d5da62b122b9456218e58f9a
ruck314 <ruck314@users.noreply.github.com>
2021-01-05 17:53:29 +0000

    deploy: 737ab5d8422f7336d87e5f6ef014eb03b3d2855a

---
+++ navtreedata.js
@@ -0,0 +1,50 @@
+/*
+@licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2019 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "SURF", "index.html", [
+    [ "Design Unit List", "annotated.html", [
+      [ "Design Unit List", "annotated.html", "annotated_dup" ],
+      [ "Design Units", "classes.html", null ],
+      [ "Design Unit Hierarchy", "hierarchy.html", null ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classEthTimingModule_1_1rtl.html#ae3f2610520504f07254ee38a4fe27f68",
+"classEvrV1EventReceiver_1_1rtl.html#a3b9616639148f7a3a805b6c896fb01f6",
+"classEvrV2Cache_1_1mapping.html#a117d2e5b8b6279f15328f47acdd4ad76",
+"classEvrV2FromV1.html#aa5fefbb58d6204d5242dd2f4be3352e0",
+"classEvrV2__tb_1_1mapping.html#ac97222cfe8410d59cd08b2fcf3325038",
+"classTPGMiniEdef_1_1TPGMiniEdefImpl.html#a832f8400160dd51face7024ff2bfaa9f",
+"classTimingCore.html#a27ed01a18a19c742fcce9b90d8d22c25",
+"classTimingGtCoreWrapper_1_1rtl.html#a049e44bbf215e36cea1560a83ca89184",
+"classTimingRx.html#a22f8db74a5a4c77e7e2767279ca36e7f",
+"classTimingStreamRx.html#a10a0ab621ea8c1ebe2adf690728ac7e4"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
