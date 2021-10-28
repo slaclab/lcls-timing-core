9dbf4d0b79290670299917a2b127e6e30a4f8b50
Larry Ruckman <ruckman@slac.stanford.edu>
2021-10-27 19:06:03 -0700

    bug fix for .tcl

--- LCLS-II/gtyUltraScale+/ruckus.tcl
+++ LCLS-II/gtyUltraScale+/ruckus.tcl
@@ -1,18 +1,26 @@
 # Load RUCKUS environment and library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
-if { $::env(VIVADO_VERSION) >= 2021.1 } {
+loadSource -lib lcls_timing_core -dir "$::DIR_PATH/rtl"
 
-   loadSource -lib lcls_timing_core -dir "$::DIR_PATH/rtl"
+# Standard 317 MHz reference OSC
+if { [info exists ::env(TIMING_246MHz)] != 1 || $::env(TIMING_246MHz) == 0 } {
+   set path "$::DIR_PATH/coregen"
 
-   # Standard 317 MHz reference OSC
-   if { [info exists ::env(TIMING_246MHz)] != 1 || $::env(TIMING_246MHz) == 0 } {
-      set path "$::DIR_PATH/coregen"
+# Standard 245.76 MHz reference OSC
+} else {
+   set path "$::DIR_PATH/coregen/smurf"
+}
 
-   # Standard 245.76 MHz reference OSC
-   } else {
-      set path "$::DIR_PATH/coregen/smurf"
-   }
+if { $::env(VIVADO_VERSION) >= 2021.1 && [info exists ::env(TIMING_246MHz)] != 1} {
+
+   loadSource -lib lcls_timing_core   -path "${path}/TimingGty_extref.dcp"
+   # loadIpCore -path "${path}/TimingGty_extref.xci"
+
+   loadSource -lib lcls_timing_core   -path "${path}/TimingGty_fixedlat.dcp"
+   # loadIpCore -path "${path}/TimingGty_fixedlat.xci"
+
+} elseif { $::env(VIVADO_VERSION) >= 2020.2 && [info exists ::env(TIMING_246MHz)] == 1 } {
 
    loadSource -lib lcls_timing_core   -path "${path}/TimingGty_extref.dcp"
    # loadIpCore -path "${path}/TimingGty_extref.xci"
