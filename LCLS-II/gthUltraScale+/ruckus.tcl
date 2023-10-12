21fc2d9fec53cd70bf05c55be95ec26d906dfc5b
Julian Mendez <jumdz@slac.stanford.edu>
2023-10-12 11:24:33 -0700

    use xci to upgrade the ip with the latest version of vivado

--- LCLS-II/gthUltraScale+/ruckus.tcl
+++ LCLS-II/gthUltraScale+/ruckus.tcl
@@ -5,11 +5,11 @@ if { $::env(VIVADO_VERSION) >= 2021.2 } {
 
    loadSource -lib lcls_timing_core -dir "$::DIR_PATH/rtl"
 
-   loadSource -lib lcls_timing_core   -path "$::DIR_PATH/coregen/TimingGth_extref.dcp"
-   # loadIpCore -path "$::DIR_PATH/coregen/TimingGth_extref.xci"
+   #loadSource -lib lcls_timing_core   -path "$::DIR_PATH/coregen/TimingGth_extref.dcp"
+   loadIpCore -path "$::DIR_PATH/coregen/TimingGth_extref.xci"
 
-   loadSource -lib lcls_timing_core   -path "$::DIR_PATH/coregen/TimingGth_fixedlat.dcp"
-   # loadIpCore -path "$::DIR_PATH/coregen/TimingGth_fixedlat.xci"
+   #loadSource -lib lcls_timing_core   -path "$::DIR_PATH/coregen/TimingGth_fixedlat.dcp"
+   loadIpCore -path "$::DIR_PATH/coregen/TimingGth_fixedlat.xci"
 
 } else {
    puts "\n\nWARNING: $::DIR_PATH requires Vivado 2021.2 (or later)\n\n"
