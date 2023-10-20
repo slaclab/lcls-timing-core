294e4688a4e9cd0eddbb51619e54ced22c4039f5
Julian Mendez <jumdz@slac.stanford.edu>
2023-10-20 16:15:44 -0700

    update IPs and remove dcp

--- LCLS-II/gthUltraScale+/ruckus.tcl
+++ LCLS-II/gthUltraScale+/ruckus.tcl
@@ -1,16 +1,11 @@
 # Load RUCKUS environment and library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
-if { $::env(VIVADO_VERSION) >= 2021.2 } {
-
+if { $::env(VIVADO_VERSION) >= 2023.1 } {
    loadSource -lib lcls_timing_core -dir "$::DIR_PATH/rtl"
-
-   loadSource -lib lcls_timing_core   -path "$::DIR_PATH/coregen/TimingGth_extref.dcp"
-   #loadIpCore -path "$::DIR_PATH/coregen/TimingGth_extref.xci"
-
-   loadSource -lib lcls_timing_core   -path "$::DIR_PATH/coregen/TimingGth_fixedlat.dcp"
-   #loadIpCore -path "$::DIR_PATH/coregen/TimingGth_fixedlat.xci"
+   loadIpCore -path "$::DIR_PATH/coregen/TimingGth_extref.xci"
+   loadIpCore -path "$::DIR_PATH/coregen/TimingGth_fixedlat.xci"
 
 } else {
-   puts "\n\nWARNING: $::DIR_PATH requires Vivado 2021.2 (or later)\n\n"
+   puts "\n\nWARNING: $::DIR_PATH requires Vivado 2023.1 (or later)\n\n"
 }
