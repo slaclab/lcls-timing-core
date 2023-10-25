9e5fa2b162c9c5c51002f74bea5992d0e0503cb8
Julian Mendez <jumdz@slac.stanford.edu>
2023-10-24 17:46:33 -0700

    back to vivado2022.2 - xci does not change

--- LCLS-II/gthUltraScale+/ruckus.tcl
+++ LCLS-II/gthUltraScale+/ruckus.tcl
@@ -1,11 +1,11 @@
 # Load RUCKUS environment and library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
-if { $::env(VIVADO_VERSION) >= 2023.1 } {
+if { $::env(VIVADO_VERSION) >= 2022.2 } {
    loadSource -lib lcls_timing_core -dir "$::DIR_PATH/rtl"
    loadIpCore -path "$::DIR_PATH/coregen/TimingGth_extref.xci"
    loadIpCore -path "$::DIR_PATH/coregen/TimingGth_fixedlat.xci"
 
 } else {
-   puts "\n\nWARNING: $::DIR_PATH requires Vivado 2023.1 (or later)\n\n"
+   puts "\n\nWARNING: $::DIR_PATH requires Vivado 2022.2 (or later)\n\n"
 }
