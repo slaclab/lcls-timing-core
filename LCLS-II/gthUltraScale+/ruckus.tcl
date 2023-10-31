e0c8b590570500a522d8f1422cbf9f646c5dc8ee
Julian Mendez <jumdz@slac.stanford.edu>
2023-10-31 13:03:23 -0700

    update ip

--- LCLS-II/gthUltraScale+/ruckus.tcl
+++ LCLS-II/gthUltraScale+/ruckus.tcl
@@ -6,10 +6,10 @@ if { $::env(VIVADO_VERSION) >= 2022.2} {
    loadSource -lib lcls_timing_core -dir "$::DIR_PATH/rtl"
 
    #loadSource -lib lcls_timing_core   -path "$::DIR_PATH/coregen/TimingGth_extref.dcp"
-   loadIpCore -path "$::DIR_PATH/coregen/TimingGth_extref.xci"
+   #loadIpCore -path "$::DIR_PATH/coregen/TimingGth_extref.xci"
 
    #loadSource -lib lcls_timing_core   -path "$::DIR_PATH/coregen/TimingGth_fixedlat.dcp"
-   loadIpCore -path "$::DIR_PATH/coregen/TimingGth_fixedlat.xci"
+   #loadIpCore -path "$::DIR_PATH/coregen/TimingGth_fixedlat.xci"
    
    #loadSource -lib lcls_timing_core -dir "$::DIR_PATH/rtl"
    #loadIpCore -path "$::DIR_PATH/coregen/TimingGth_extref.xci"
